ccb93cda528aef857f24e7bf07c6fd98f8c5cc78
kamkry-zz <kamkry18@gmail.com>
2020-09-09 19:28:23 +0200

    {10.IX.2020r.} Readme.md added
    First operational version of API. Can be controlled via CLI like.: Powershell.
    
    Tested on FX-8320e with RX-480 GPU.

---
+++ CoreAPI.py
@@ -0,0 +1,67 @@
+# Simple script to automate transcoding videos to H265 using ffmpeg pack
+# As for current version, it is dedicated to AMD GPU's
+# Created by Kamil Krysiak <kamkry18@gmail.com>
+#
+
+import os                                                       # library for capturing OS type/verison
+import subprocess                                               # library for handling Powershell <-> Python communication
+import time                                                     # library for handling time (to be able to wait :) )
+import pathlib                                                  # library for opening paths using '\'
+
+subprocess.run(["powershell.exe", "pip install string-color"])  # install coloring feature for Python3
+subprocess.run(["powershell.exe", "pip install pymediainfo"])   # install video metadata read module for Python3
+
+# Dedicated to PowerShell in Windows
+print()
+print()
+
+from stringcolor import *
+# color-ify things a bit
+print('Welcome to simple Python script for easy, seamless transition into H265-coded videos!')
+system_name = os.name
+print('Your OS is: ' + cs(system_name, "Blue"))
+time.sleep(1)
+
+print('Will use FFMPEG codec pack for encoding')
+print('Type-in path with videos to be transcoded: ', end = '')
+path_input = input()                                            # Path to directory containing video file
+
+print('Type-in name of file to be transcoded: ', end = '')
+file = input()                                                  # Path to particular video file
+from pymediainfo import MediaInfo
+media_info = MediaInfo.parse(str(path_input) + "/" + str(file))
+
+for track in media_info.tracks:
+    if track.track_type == 'Video':                             # Preapring info about current video bitrate
+      break
+
+print('Current video bitrate: ' + cs(str(track.bit_rate / 1000000), "Blue") + ' Mbps')
+print("Type-in target VIDEO bitrate value (in Mbps): ", end = '')
+target_bitrate_video = input()                                  # Declaration of targeted video bitrate
+
+    # ADD IMPLEMENTATION FOR RETRIVING CURRENT AUDIO BITRATE AND NO. OF CHANNELS
+
+print("Type-in target AUDIO bitrate value (in kbps): ", end = '')
+target_bitrate_audio = input()                                  # Declaration of targeted audio bitrate
+
+print('Please confirm all settings: ', end = '')
+print('Source file: ' + cs(str(path_input), "DeepPink3") + cs("/", "DeepPink3") + cs(str(file), "DeepPink3"))
+time.sleep(2)
+
+print('Target video bitrate: ' + cs(str(target_bitrate_video), "DeepPink3") + cs(" Mbps", "DeepPink3"))
+print('Target audio bitrate: ' + cs(str(target_bitrate_audio), "DeepPink3") + cs(" kbps", "DeepPink3"))
+print("Is that correct? " + cs("Type 'yes': ", "Red"), end = '')
+confirmation = input()
+
+if str(confirmation) == "yes":
+    from pathlib import Path
+    command = Path("ffmpeg -hwaccel auto -i " + str(path_input) + "/" + str(file) + " -c:v hevc_amf -preset medium -b:v " + str(target_bitrate_video) + "M -c:a aac -b:a " + str(target_bitrate_audio) + "k " + str(path_input) + "/" + r"_h265/" + str(file))
+    #file_to_execute = open(command)
+    print(str(command))
+    subprocess.run(["powershell.exe", str(command)])
+
+else:
+
+    print('Terminating')
+
+exit
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # AmdGPUH265Handler
 A simple Python automation for AMD GPU-accelerated video encoding using H265 via ffmpeg
+
+Description of functionalities:
+* As for now .py script is capable of handling whatever ffmpeg (build: ffmpeg-20200831-4a11a6f-win64-static) can do - which is a lot. For 
+more info go to <https://ffmpeg.org/documentation.html>
+
+* Native support for AMD GPUs arise from the use of AMD AMF H.265 (HEVC) Encoder - hevc_amf  .
+
+* Compared to software-driven encoding of H264 -> H265 this speeds-up overall process time from 5 up to 12 times.
+
+* For the moment CLI is the only way to run and interact with the script.
+
+-------------------------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------------------------
+
+Script requires Python 3.8 . To run it simply execute it with IDLE.
+
+First what can be seen is the installation of additional py modules used in the script. After that name of OS is diplayed
+
+Upon execution of script, usr has to:
+
+0. In the directory where source video is stored, create a "_h265" folder
+1. Input path for the directory in which source video is stored
+2. Name of the video to be encoded WITH FILE EXTENSION - e.g. Video.mp4 or Movie.mkv
+3. Value for destination video stream bitrate. In the line above there is a value of source media video bitrate.
+4. Value for destination audio stream bitrate. Current number of audio channels will remain after encoding.
+5. Check and confirm by typing "yes" that all values, path and filename are correct.
+6. Wait for the process to end. At the right-most end of last line there's a current speed displayed - that means how may times faster 
+(or slower) encoding will take related to normal play (x1.0) time. There's also a current time-stamp displayed.
+7. Your encoded video will be in ..\<source video folder\_h265\<original filename>.<original container extension>
+
+Thats it! Enjoy smaller-but-almost-the-same-quality video :)
+Kamil Krysiak <kamkry18@gmail.com>
